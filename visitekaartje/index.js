6a398f193d94d8be9d90282869a2c01dc26b2c7d
Koen Haagsma <koen.haagsma@themobilecompany.com>
2022-02-07 14:25:42 +0100

    Styling changes

--- visitekaartje/index.js
+++ visitekaartje/index.js
@@ -3,11 +3,26 @@
 // Open content tab
 const icons = document.querySelectorAll('.icon');
 const content = document.querySelectorAll('.content-container');
+const iconContainer = document.querySelector('.icon-container');
+console.log(iconContainer);
 icons.forEach((icon) => {
     icon.onclick = () => {
         [...contentArray] = content;
         contentArray[parseInt(icon.classList[icon.classList.length - 1]) - 1].classList.toggle(
             'content-container-shown',
         );
+        iconContainer.classList.toggle('icon-container-shown');
+    };
+});
+
+// Close content tab
+const closeButtons = document.querySelectorAll('.close');
+closeButtons.forEach((button) => {
+    button.onclick = () => {
+        [...contentArray] = contentArray;
+        contentArray[parseInt(button.classList[button.classList.length - 1]) - 1].classList.toggle(
+            'content-container-shown',
+        );
+        iconContainer.classList.toggle('icon-container-shown');
     };
 });
