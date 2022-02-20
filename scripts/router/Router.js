a04b032a7698a9ca5d8bbe18bf1995511d115559
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-02-20 21:50:30 +0100

    Fixing github pages

--- scripts/router/Router.js
+++ scripts/router/Router.js
@@ -1,6 +1,16 @@
 import Utils from '../helpers/Utils.js';
 import Error404 from '../views/Error404.js';
-import routes from './routes.js';
+import Home from '../views/Home.js';
+import ManualInput from '../views/ManualInput.js';
+import Details from '../views/Details.js';
+import Scanner from '../views/Scanner.js';
+
+const routes = {
+    '/': Home,
+    '/manual': ManualInput,
+    '/details': Details,
+    '/scanner': Scanner,
+};
 
 const router = async () => {
     const content = null || document.getElementById('page_container');
