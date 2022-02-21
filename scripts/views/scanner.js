640d268cce8d7f162306b5fdff36d56870205372
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-02-21 18:21:54 +0100

    Made SPA and dynamic pages

--- scripts/views/scanner.js
+++ scripts/views/scanner.js
@@ -1,13 +1,41 @@
+import { detect } from '../modules/detect.js';
+import { fetchData } from '../modules/fetch.js';
+
 const Scanner = {
     render: async () => {
         const view = `
             <section class="section">
-                <h1> Scanner </h1>
+                <div class="container">
+                    <button>Start scanning</button>
+                    <button>Stop scanning</button>
+                </div>
             </section>
         `;
         return view;
     },
-    after_render: async () => {},
+    after_render: async () => {
+        const buttons = document.querySelectorAll('button');
+        const div = document.querySelector('.container');
+        buttons[0].onclick = (event) => {
+            event.preventDefault;
+            detect.start(`.${div.className}`, (result) => {
+                fetchData(result).then((data) => {
+                    if (data.status === 1) {
+                        console.log('Found!');
+                        console.log(data);
+                    } else if (data.status === 0) {
+                        console.log('Not Found!');
+                        console.log(data);
+                    }
+                });
+            });
+        };
+
+        buttons[1].onclick = (event) => {
+            event.preventDefault;
+            detect.stop(`.${div.className}`);
+        };
+    },
 };
 
 export default Scanner;
